41605486d982abdc6270751d4f1b3ce22dd50016
jiylee <jjenny4711@gmail.com>
2024-05-23 16:17:17 -0400

    e

--- controller/authController.js
+++ controller/authController.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY;
 authController.authenticate = (req, res,next) => {
   try{
-    const tokenString = req.headers.authorization.split
+    const tokenString = req.headers.authorization
     if(!tokenString) throw new Error('No token provided');
-    const token = tokenString.replace("Bearer ","");
+    const token = tokenString.split(' ')[1];
     jwt.verify(token,JWT_SECRET_KEY,(error,payload)=>{
       if(error) throw new Error('Invalid token');
       console.log(payload,'payload!!!!!!!!!')
