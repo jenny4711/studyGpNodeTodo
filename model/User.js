2c4274a2acd186120c50e09ef7fb40ccd90a3a62
jiylee <jjenny4711@gmail.com>
2024-05-22 20:28:48 -0400

    e

--- model/User.js
+++ model/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
